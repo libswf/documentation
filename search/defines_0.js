143c58251578d04236387ef75c4708c967811a0c
Rodger Combs <rodger.combs@gmail.com>
2015-05-03 20:48:50 -0500

    Update docs

---
+++ search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['lzma_5fheader_5fsize',['LZMA_HEADER_SIZE',['../parser_8c.html#a7521435573132c360a6404c19d2efb83',1,'parser.c']]]
+];
---
+++ search/searchdata.js
@@ -0,0 +1,39 @@
+var indexSectionsWithContent =
+{
+  0: "abcefhijlmnprstvxy",
+  1: "bs",
+  2: "bips",
+  3: "s",
+  4: "abcefhijlmnprstvxy",
+  5: "s",
+  6: "s",
+  7: "ps",
+  8: "l"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros"
+};
+
